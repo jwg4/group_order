13d4cdc1c652d389c487d78bcccca8f266684e13
Jack Grahl <jack@prismfp.com>
2018-09-06 15:29:32 +0100

    Test multiplication.

--- test_group.py
+++ test_group.py
@@ -7,5 +7,11 @@ class TestDihedralGroup(unittest.TestCase):
     def setUp(self):
         self.grp = Group(DIHEDRAL_8)
 
+    def test_ba_plus_b(self):
+        ba = self.grp.make((1, 0))
+        b = self.grp.make((1,))
+        ca = self.grp.make((2, 0))
+        self.assertEqual(ba + b, ca)
+
     def test_commutator_subgroup(self):
         self.assertEqual(self.grp.commutator_subgroup, [(), (2)])
