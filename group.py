e910aa54b3b5db365af24472d7a9ac443bdc55c4
Jack Grahl <jack@prismfp.com>
2018-09-06 15:29:49 +0100

    Define repr to be the same as str - for now.

--- group.py
+++ group.py
@@ -17,6 +17,9 @@ def element_class(group):
         def __str__(self):
             return "".join([ALPHA[i] for i in self.representation])
 
+        def __repr__(self):
+            return self.__str__()
+
         def __eq__(self, other):
             return self.representation == other.representation
 
